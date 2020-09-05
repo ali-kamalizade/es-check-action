59f477ebf99e42f7d210b11fb354b9e0df6ac920
ali-kamalizade <a.kamikaze@hotmail.de>
2020-09-05 10:48:39 +0200

    Add first unit test

---
+++ example-dist/es5.js
@@ -0,0 +1,3 @@
+function iCauseIssue() {
+	return null;
+}
---
+++ src/execute-github-action.test.ts
@@ -0,0 +1,79 @@
+import * as core from "@actions/core";
+import {executeGitHubAction} from './execute-github-action';
+import {GithubActionInputType} from "./action.interface";
+import * as github from '@actions/github';
+
+describe('GitHub Action', () => {
+
+	let spyGetInput: jasmine.Spy;
+	let spySetFailed: jasmine.Spy;
+
+	beforeEach(() => {
+		spyGetInput = spyOn(core, 'getInput');
+		spySetFailed = spyOn(core, 'setFailed');
+	});
+
+	describe('Success cases', () => {
+
+		it('should work if directory exists, ES version is valid and specified files work for specified ES version', () => {
+			spyAndReturnInputValue('example-dist', 'es5', '**/*es5*.js');
+			executeGitHubAction();
+
+			expect(spySetFailed).not.toHaveBeenCalled();
+		});
+
+		it('should work if all files work for specified ES version', () => {
+			spyAndReturnInputValue('example-dist', 'es2015', '*.js');
+			executeGitHubAction();
+
+			expect(spySetFailed).not.toHaveBeenCalled();
+		});
+
+	});
+
+	describe('Error cases', () => {
+
+		it('should fail if directory does not exist', () => {
+			spyAndReturnInputValue('does-not-exist', 'es5', '**/*es5*.js');
+			executeGitHubAction();
+
+			expect(spySetFailed).toHaveBeenCalled();
+		});
+
+		it('should fail if ES version is invalid', () => {
+			spyAndReturnInputValue('example-dist', 'error', '**/*es5*.js');
+			executeGitHubAction();
+
+			expect(spySetFailed).toHaveBeenCalled();
+		});
+
+		it('should fail if files do not work for specified ES version', () => {
+			spyAndReturnInputValue('example-dist', 'es5', '**/*.js');
+			executeGitHubAction();
+
+			expect(spySetFailed).toHaveBeenCalled();
+		});
+
+		it('should fail if there is an error after es-check was run successfully', () => {
+			spyOn(JSON, 'stringify').and.throwError('Simulating GitHub Action payload fail');
+			spyAndReturnInputValue('example-dist', 'es5', '**/*es5*.js');
+			executeGitHubAction();
+
+			expect(spySetFailed).toHaveBeenCalled();
+		});
+	})
+
+	function spyAndReturnInputValue(dist: string = '', ecmaVersion: string = '', filesGlob = '') {
+		// input corresponds with GitHub Action input names
+		spyGetInput.and.callFake((input: GithubActionInputType) => {
+			switch (input) {
+				case 'directory':
+					return dist;
+				case 'ecmaVersion':
+					return ecmaVersion;
+				case 'files':
+					return filesGlob;
+			}
+		});
+	}
+});
