15c5ef7dc87a84b91ad457cd3a86e1597d224878
ali-kamalizade <a.kamikaze@hotmail.de>
2020-09-05 11:44:16 +0200

    Remove unnecessary code

--- src/execute-github-action.ts
+++ src/execute-github-action.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 import {execSync} from 'child_process';
 import {GithubActionInputType} from './action.interface';
 
@@ -14,13 +13,6 @@ export function executeGitHubAction() {
 	try {
 		console.info(`Checking files in: ${inputs.pathToDistFolder()}!`);
 		execSync(`npx es-check ${inputs.ecmaVersion()} ./${inputs.pathToDistFolder()}/${inputs.filesGlob()} --verbose`);
-	} catch (error) {
-		console.error(error.message);
-		core.setFailed(error.message);
-	}
-	try {
-		const payload = JSON.stringify(github.context.payload, undefined, 2);
-		console.log(`The event payload: ${payload}`);
 	} catch (error) {
 		console.error(error.message);
 		core.setFailed(error.message);
